4c64d52592e2875cc52f0e4b048086dfb8e92eb4
Namrata-1487 <namrata.shirsaat@gmail.com>
2024-11-27 20:52:49 +0530

    base ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Container, Nav, Navbar, Row, Col, Dropdown, DropdownItem,NavLink } from 'react-bootstrap';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container className="bg-light container-fluid w-auto p-0 m-0">
+        <Navbar className="m-0 p-3  bg-dark w-100">
+          <Row className="bg-dark m-0 w-100">
+            <Col className="col-9">
+              <Navbar.Brand className="fw-bold text-white w-100 fs-3 ps-5" href="/">
+                logo
+              </Navbar.Brand></Col>
+            <Col className=" col-3 "> <Nav className="m-0 p-0 justify-content-center">
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  categories
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <DropdownItem href="/">Geneal</DropdownItem>
+                  <DropdownItem href="/">Technology</DropdownItem>
+                  <DropdownItem href="/">Business</DropdownItem>
+                  <DropdownItem href="/">Health</DropdownItem>
+                  <DropdownItem href="/">Sports</DropdownItem>
+                  <DropdownItem href="/">Entertainment</DropdownItem>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Nav></Col>
+          </Row>
+        </Navbar>
+        <Container className="w-100 m-0 p-0">
+          <Row className="p-0 m-0 sidebarcontainer row d-flex">
+            {/* verticle links  */}
+            <Col className="bg-dark p-2 m-0 sidebar col-2">
+              <NavLink className="fs-2 text-white nav-link mb-5 p-2">General</NavLink>
+              <NavLink className="fs-2 text-white nav-link mb-5 p-2" >Technology</NavLink>
+              <NavLink className="fs-2 text-white nav-link mb-5 p-2" >Business</NavLink>
+              <NavLink className="fs-2 text-white nav-link mb-5 p-2" >Health</NavLink>
+              <NavLink className="fs-2 text-white nav-link mb-5 p-2" >Sports</NavLink>
+              <NavLink className="fs-2 text-white nav-link mb-5 p-2" >Entertainment</NavLink>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+
     </>
   )
 }
 
-export default App
+export default App;
